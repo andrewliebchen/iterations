c0a4c218ccfb681c5ec3c0b04f09931b9848cb17
Andrew Liebchen <andrewliebchen@gmail.com>
2019-05-22 22:13:46 -0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import _ from "lodash";
 
-function App() {
+const App = props => {
+  const [iterations, setIterations] = useState(1);
+  const [initialValue, setInitialValue] = useState(4);
+  const [increment, setIncrement] = useState(4);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div>
+        <span>Do </span>
+        <input
+          type="number"
+          placeholder="Iterations"
+          value={iterations}
+          onChange={event => setIterations(parseInt(event.target.value))}
+        />
+        <span> iterations starting with </span>
+        <input
+          type="number"
+          placeholder="Starting value"
+          value={initialValue}
+          onChange={event => setInitialValue(parseInt(event.target.value))}
+        />
+        <span> and increment by </span>
+        <input
+          type="number"
+          placeholder="Increment"
+          value={increment}
+          onChange={event => setIncrement(parseInt(event.target.value))}
+        />
+      </div>
+      {_.times(iterations, i => (
+        <div
+          key={i}
+          style={{
+            margin: 20,
+            border: "1px solid",
+            display: "inline-block",
+            padding: increment * i + initialValue
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Example
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
