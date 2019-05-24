0eb70e0e4704c0b9903036c456b36927f1c2aaf4
Andrew Liebchen <andrewliebchen@gmail.com>
2019-05-23 21:42:18 -0700

    Iterations array

--- src/App.js
+++ src/App.js
@@ -1,68 +1,81 @@
 import React, { useState } from "react";
 import _ from "lodash";
 
+const defaultIteration = {
+  property: "padding",
+  initialValue: 4,
+  increment: 4
+};
+
 const App = props => {
   const [count, setCount] = useState(1);
-  const [iteration, setIteration] = useState({
-    property: "padding",
-    initialValue: 4,
-    increment: 4
-  });
+  const [iterations, setIterations] = useState([defaultIteration]);
 
   return (
     <div>
       <div>
-        <span>Do </span>
-        <input
-          type="number"
-          placeholder="Iterations"
-          value={count}
-          onChange={event => setCount(parseInt(event.target.value))}
-        />
-        <span> iterations of </span>
-        <select
-          value={iteration.property}
-          onChange={event =>
-            setIteration({ ...iteration, property: event.target.value })
-          }
+        <div>
+          <input
+            type="number"
+            placeholder="Iterations"
+            value={count}
+            onChange={event => setCount(parseInt(event.target.value))}
+          />
+          <span> iterations</span>
+        </div>
+
+        {iterations.map((iteration, i) => (
+          <div key={i}>
+            <select
+              value={iteration.property}
+              onChange={event =>
+                setIterations({ ...iteration, property: event.target.value })
+              }
+            >
+              <option value="borderRadius">border-radius</option>
+              <option value="fontSize">font-size</option>
+              <option value="padding">padding</option>
+            </select>
+            <span> starting with </span>
+            <input
+              type="number"
+              placeholder="Starting value"
+              value={iteration.initialValue}
+              onChange={event =>
+                setIterations({
+                  ...iteration,
+                  initialValue: parseInt(event.target.value)
+                })
+              }
+            />
+            <span> and increment by </span>
+            <input
+              type="number"
+              placeholder="Increment"
+              value={iteration.increment}
+              onChange={event =>
+                setIterations({
+                  ...iteration,
+                  increment: parseInt(event.target.value)
+                })
+              }
+            />
+          </div>
+        ))}
+        <button
+          onClick={() => setIterations([...iterations, defaultIteration])}
         >
-          <option value="borderRadius">border-radius</option>
-          <option value="fontSize">font-size</option>
-          <option value="padding">padding</option>
-        </select>
-        <span> starting with </span>
-        <input
-          type="number"
-          placeholder="Starting value"
-          value={iteration.initialValue}
-          onChange={event =>
-            setIteration({
-              ...iteration,
-              initialValue: parseInt(event.target.value)
-            })
-          }
-        />
-        <span> and increment by </span>
-        <input
-          type="number"
-          placeholder="Increment"
-          value={iteration.increment}
-          onChange={event =>
-            setIteration({
-              ...iteration,
-              increment: parseInt(event.target.value)
-            })
-          }
-        />
+          Add
+        </button>
       </div>
       {_.times(count, i => {
-        const { property, increment, initialValue } = iteration;
+        const { property, increments, initialValue } = iterations[0];
         let style = {
           margin: 20,
           border: "1px solid",
           display: "inline-block"
         };
-        style[property] = increment * i + initialValue;
+        style[property] = increments * i + initialValue;
 
         return (
           <div key={i} style={style}>
